b5cc8715b8ca70dce2b3a22479aad8c205a9ec76
Mudit Gulgulia <gulgulia17@gmail.com>
2023-09-27 17:36:15 +0530

    Minimal Helpers Added

---
+++ RedshiftHelper.py
@@ -0,0 +1,166 @@
+import redshift_connector
+
+
+class RedshiftHelper:
+    def __init__(self, host, user, password, database, port=5439):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.port = port
+        self.connection = None
+        self.cursor = None
+        self.conditions = []
+        self.selected_columns = "*"
+        self.group_by_columns = None
+
+    def connect(self):
+        """
+        Establish a connection to the Redshift cluster.
+        """
+        self.connection = redshift_connector.connect(
+            host=self.host,
+            database=self.database,
+            user=self.user,
+            password=self.password,
+            port=self.port
+        )
+        self.cursor = self.connection.cursor()
+
+    def disconnect(self):
+        """
+        Disconnect from the Redshift cluster.
+        """
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
+
+    def select(self, columns):
+        """
+        Specify the columns to select in the query.
+        """
+        if columns:
+            self.selected_columns = ", ".join(columns)
+        return self  # Return self to enable method chaining
+
+    def where(self, column, value, operator='='):
+        """
+        Add a WHERE condition to the query.
+        """
+        self.conditions.append((column, operator, value))
+        return self  # Return self to enable method chaining
+
+    def whereBetween(self, column, start_value, end_value):
+        """
+        Add a WHERE BETWEEN condition to the query.
+        """
+        self.conditions.append(
+            (column, 'BETWEEN', f"'{start_value}' AND '{end_value}'"))
+        return self  # Return self to enable method chaining
+
+    def groupBy(self, *columns):
+        """
+        Specify columns to use in the GROUP BY clause.
+        """
+        self.group_by_columns = ", ".join(columns)
+        return self  # Return self to enable method chaining
+
+    def build_query(self, table_name):
+        """
+        Build the SQL query with the specified conditions, selected columns, and GROUP BY clause.
+        """
+        condition_parts = []
+        for column, operator, value in self.conditions:
+            condition_parts.append(f"{column} {operator} %s")
+
+        condition_str = " AND ".join(condition_parts)
+
+        query = f"SELECT {self.selected_columns} FROM {table_name}\n"
+        if condition_str:
+            query += f"WHERE {condition_str}\n"
+        if hasattr(self, 'group_by_columns'):
+            query += f"GROUP BY {self.group_by_columns}\n"
+        return query
+
+    def count(self, table_name):
+        """
+        Perform a COUNT(*) operation on the specified table with conditions.
+        """
+        query = self.build_query(table_name)
+        self.cursor.execute(query, [val for _, val in self.conditions])
+        return self.cursor.fetchone()[0]
+
+    def sql(self, table_name):
+        query = self.build_query(table_name)
+        values = [val for _, _, val in self.conditions]
+
+        # Manually construct the query with parameter values
+        sql = query % tuple(values)
+
+        return sql
+
+    def _fetch_results(self, query):
+        """
+        Execute the query and fetch results as dictionaries.
+        """
+        self.cursor.execute(query)
+        columns = [desc[0] for desc in self.cursor.description]
+        results = []
+        for row in self.cursor.fetchall():
+            result_dict = {col: value for col, value in zip(columns, row)}
+            results.append(result_dict)
+        return results
+
+    def get(self, table_name):
+        """
+        Execute the query with conditions and return the results as dictionaries.
+        """
+        query = self.sql(table_name)
+        return self._fetch_results()
+
+    def first(self, table_name):
+        """
+        Return only the first record from a given table as a dictionary.
+        """
+        query = f"{self.sql(table_name)} LIMIT 1;"
+        results = self._fetch_results(query)
+        return results[0] if results else None
+
+    def update(self, table_name, column_values):
+        """
+        Update records in a given table with the specified column-value pairs and conditions.
+        """
+        set_columns = ", ".join(
+            [f"{col} = %s" for col, _ in column_values.items()])
+        query = f"UPDATE {table_name} SET {set_columns}\n"
+        if self.conditions:
+            condition_str = " AND ".join(
+                [f"{col} = %s" for col, _ in self.conditions])
+            query += f"WHERE {condition_str}\n"
+        values = [val for _, val in column_values.values()] + \
+            [val for _, val in self.conditions]
+        self.cursor.execute(query, values)
+        self.connection.commit()
+        return self.cursor.rowcount
+
+    def delete(self, table_name):
+        """
+        Delete records from a given table based on a condition.
+        """
+        query = f"DELETE FROM {table_name}\n"
+        if self.conditions:
+            condition_str = " AND ".join(
+                [f"{col} = %s" for col, _ in self.conditions])
+            query += f"WHERE {condition_str}\n"
+        self.cursor.execute(query, [val for _, val in self.conditions])
+        self.connection.commit()
+        return self.cursor.rowcount
+
+    def raw(self, query):
+        """
+        Execute a custom SQL query.
+        """
+        self.cursor.execute(query)
+        self.connection.commit()
+        return self.cursor.rowcount
---
+++ readme.md
@@ -0,0 +1,105 @@
+# RedshiftHelper
+
+## Overview
+
+The `RedshiftHelper` class is a Python utility for working with Amazon Redshift, a data warehousing service. This class provides methods to simplify the construction of SQL queries and operations on Redshift tables.
+
+## Usage
+
+### Initialization
+
+To use the `RedshiftHelper` class, you need to initialize it by providing the necessary connection parameters:
+
+```python
+from redshift_connector import RedshiftHelper
+
+redshift = RedshiftHelper(
+    host='your_redshift_host',
+    user='your_username',
+    password='your_password',
+    database='your_database_name',
+    port=5439  # Default Redshift port
+)
+```
+
+### Connecting to Redshift
+
+You can establish a connection to the Redshift cluster using the `connect` method:
+
+```python
+redshift.connect()
+```
+
+### Building Queries
+
+The `RedshiftHelper` class allows you to build SQL queries easily. You can specify the columns to select, add WHERE conditions, and specify GROUP BY columns:
+
+```python
+redshift.select(['column1', 'column2']).where('column3', 'value').groupBy('column4')
+```
+
+### Executing Queries
+
+You can execute various types of queries, such as `select`, `count`, `update`, `delete`, and custom SQL queries using the respective methods:
+
+```python
+# Execute a SELECT query and fetch results as dictionaries
+results = redshift.get('your_table_name')
+
+# Perform a COUNT(*) operation
+count = redshift.count('your_table_name')
+
+# Update records in the table
+redshift.update('your_table_name', {'column1': 'new_value'})
+
+# Delete records from the table
+redshift.delete('your_table_name')
+
+# Execute a custom SQL query
+redshift.raw('your_custom_sql_query')
+```
+
+### Disconnecting from Redshift
+
+Don't forget to disconnect from the Redshift cluster when you're done:
+
+```python
+redshift.disconnect()
+```
+
+## Example
+
+Here's a simple example of how to use the `RedshiftHelper` class:
+
+```python
+redshift = RedshiftHelper(
+    host='your_redshift_host',
+    user='your_username',
+    password='your_password',
+    database='your_database_name'
+)
+
+redshift.connect()
+
+# Build and execute a SELECT query
+results = redshift.select(['column1', 'column2']).where('column3', 'value').get('your_table_name')
+
+print(results)
+
+redshift.disconnect()
+```
+
+## Note
+
+Ensure you have the `redshift_connector` library installed to use this class.
+
+```shell
+pip install redshift_connector
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+```
+
+You can replace the placeholders with your actual Redshift connection details and table names. Additionally, make sure to include the necessary library installation command at the end of the README if it's not already installed.
